aceae5dbffce5a0bff1a177488ba07330d63ef46
root <root@aa2175.puppetlabs.vm>
2016-04-06 15:39:38 +0000

    edited file class for memcached config file

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -2,8 +2,12 @@ class memcached {
   package { 'memcached':
     ensure => present,
   }
-  file { '/etc/sysconfig/memcached':
+  file { 'memcached_config' :
+    path    => '/etc/sysconfig/memcached',
     ensure  => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
     require => Package['memcached'],
     source  => 'puppet:///modules/memcached/sysconfig_memcached',
   }
