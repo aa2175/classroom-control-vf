b7b7fcd07bbeb53ecd19ac63479141443b4bfdd0
root <root@aa2175.puppetlabs.vm>
2016-04-06 15:34:56 +0000

    added memcached config file

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -2,4 +2,9 @@ class memcached {
   package { 'memcached':
     ensure => present,
   }
+  file { '/etc/sysconfig/memcached':
+    ensure  => file,
+    require => Package['memcached'],
+    source  => 'puppet:///modules/memcached/sysconfig_memcached',
+  }
 }
